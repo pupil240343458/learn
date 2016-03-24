1b251f4149aadff199a015e65db29dae73e2d51c
pupil <pupil240343458@gmail.com>
2016-03-25 00:27:34 +0800

    慕课网关于synchronized源代码

---
+++ multithread/src/syn/EnergySystemTest.java
@@ -0,0 +1,21 @@
+package syn;
+
+
+public class EnergySystemTest {
+
+	//将要构建的能量世界中能量盒子数量
+	public static final int BOX_AMOUNT = 100;
+	//每个盒子初始能量
+    public static final double INITIAL_ENERGY = 1000;
+
+    public static void main(String[] args){
+		//13到15行，new的对象有问题。如果把13行放到for循环里边会发生什么
+    	EnergySystem eng = new EnergySystem(BOX_AMOUNT, INITIAL_ENERGY);
+    	for (int i = 0; i < BOX_AMOUNT; i++){
+    		EnergyTransferTask task = new EnergyTransferTask(eng, i, INITIAL_ENERGY);
+    		Thread t = new Thread(task,"TransferThread_"+i);
+    		t.start();
+    	}
+    }
+
+}
