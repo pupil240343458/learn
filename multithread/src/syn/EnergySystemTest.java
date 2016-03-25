defe3e83e5acf1d604aeca8d6ec51ca1269e470e
pupil <pupil240343458@gmail.com>
2016-03-25 11:34:26 +0800

    解决EnergySystemTest.java中的疑惑

--- multithread/src/syn/EnergySystemTest.java
+++ multithread/src/syn/EnergySystemTest.java
@@ -9,7 +9,7 @@ public class EnergySystemTest {
     public static final double INITIAL_ENERGY = 1000;
 
     public static void main(String[] args){
-		//13到15行，new的对象有问题。如果把13行放到for循环里边会发生什么
+
     	EnergySystem eng = new EnergySystem(BOX_AMOUNT, INITIAL_ENERGY);
     	for (int i = 0; i < BOX_AMOUNT; i++){
     		EnergyTransferTask task = new EnergyTransferTask(eng, i, INITIAL_ENERGY);
