287cfeccfe804e7cdc11ced0f8c35a4eb3d321b1
pupil <pupil240343458@gmail.com>
2016-04-02 23:46:21 +0800

    通过使用spring的api实现前置后置通知

---
+++ Spring_1/src/aopApi/helloWorld.java
@@ -0,0 +1,13 @@
+package aopApi;
+
+/**
+ * Created by pupil on 2016/4/2.
+ */
+public class helloWorld {
+    public void getHelloWorld(){
+        System.out.println("helloWorld");
+    }
+    public void getHello(String string){
+        System.out.println(string);
+    }
+}
