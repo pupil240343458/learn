107368f38d14c5b373877a01edd648d9e83d506b
pupil <pupil240343458@gmail.com>
2016-04-03 13:00:45 +0800

    无法运行，spring切点方式2

---
+++ Spring_1/src/aopCustom/main.java
@@ -0,0 +1,15 @@
+package aopCustom;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by pupil on 2016/4/2.
+ */
+public class main {
+    public static void main(String[] args) {
+        ApplicationContext content=new ClassPathXmlApplicationContext("aopCustom.xml");
+        helloWorld helloWorld=(aopCustom.helloWorld)content.getBean("helloWorld");
+        helloWorld.getHelloWorld();
+    }
+}
