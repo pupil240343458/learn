6d53895e8ae74bce26d4643750cb5b6c9cc4e0c0
pupil <pupil240343458@gmail.com>
2016-05-20 00:18:58 +0800

    进一步学习struts2

---
+++ struts2_2/src/action/helloAction.java
@@ -0,0 +1,36 @@
+package action;
+
+/**
+ * Created by pupil on 2016/5/18.
+ */
+public class helloAction {
+    private String name;
+    private String passWord;
+    public String execute(){
+        if("admin".equals(name)&&"admin".equals(passWord)){
+            return "success";
+        }else {
+            return "failed";
+        }
+    }
+    public String logout(){
+        System.out.println("logout");
+        return "success";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+}
---
+++ struts2_2/web/login.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: pupil
+  Date: 2016/5/18
+  Time: 15:13
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <form action="hello" method="get">
+    name：<input type="text" name="name">
+    passWord：<input type="password" name="passWord">
+    <input type="submit" value="submit">
+  </form>
+  </body>
+</html>
